3b59ebedfaa8d507e9e3e05ac04e3616fedc9f04
JadenCooper <jaden_cooper@outlook.com>
2023-11-06 16:35:52 +1300

    Completed 173, 174, 175

--- SimpleShooter/Source/SimpleShooter/Gun.cpp
+++ SimpleShooter/Source/SimpleShooter/Gun.cpp
@@ -22,6 +22,28 @@ AGun::AGun()
 void AGun::PullTrigger()
 {
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+
+	if(OwnerPawn == nullptr) return;
+	AController* OwnerController = OwnerPawn->GetController();
+	if(OwnerController == nullptr) return;
+
+	FVector Location;
+	FRotator Rotation;
+	OwnerController->GetPlayerViewPoint(Location, Rotation);
+
+	FVector End = Location + Rotation.Vector() * MaxRange;
+
+
+	FHitResult Hit;
+	bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECollisionChannel::ECC_GameTraceChannel1);
+
+	if(bSuccess)
+	{
+		FVector ShotDirection = -Rotation.Vector();
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactEffect,Hit.Location, ShotDirection.Rotation());
+	}
 }
 
 // Called when the game starts or when spawned
