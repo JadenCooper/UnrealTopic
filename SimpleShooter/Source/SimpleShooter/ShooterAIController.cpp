8fb58f278ecb098735d2b838d8f4821d74334344
JadenCooper <jaden_cooper@outlook.com>
2023-11-08 15:43:13 +1300

    Completed 181, 182, 183, 184, 185, 186

---
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if(AIBehavior != nullptr)
+    {
+        RunBehaviorTree(AIBehavior);
+        APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+        GetBlackboardComponent()->SetValueAsVector(TEXT("Player Location"), PlayerPawn->GetActorLocation());
+    }
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+/*     APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    if (LineOfSightTo(PlayerPawn))
+    {
+        MoveToActor(PlayerPawn, AcceptanceRadius);
+        SetFocus(PlayerPawn);
+    }   
+    else
+    {
+        ClearFocus(EAIFocusPriority::Gameplay);
+        StopMovement();
+    } */
+}
